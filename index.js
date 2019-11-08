05d104d4064d40ce3a93df53d8a35b045c847385
dbe <davidbrianethier@gmail.com>
2019-11-08 17:17:37 -0500

    Removing client code

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const path = require('path');
+const express = require('express')
+const exphbs  = require('express-handlebars')
+const path = require('path')
 const request = require('request')
+const util = require('util')
 
 const OrderBook = require('./OrderBook.js').OrderBook;
 
-const app = express();
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+const app = express()
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
 const B_GET = 'https://api.bittrex.com/api/v1.1/public/getorderbook?market=BTC-ETH&type=both';
 const P_GET = 'https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH&depth=100'
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.render("home", {
       ob: ob
     })
-  });
+  })
 
     // request(P_GET, function(error, response, body) {
     //   let result = JSON.parse(body)
